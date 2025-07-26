1d5278fad979227de445c912e4ee3454c5eb266a
artem <atem.mikhailov.am@gmail.com>
2025-07-26 16:15:33 +0500

    CustomContainer and CustomContainerCompose are implemented

--- app/src/main/java/com/example/androidpracticumcustomview/XmlActivity.kt
+++ app/src/main/java/com/example/androidpracticumcustomview/XmlActivity.kt
@@ -3,37 +3,67 @@ package com.example.androidpracticumcustomview
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.view.View
 import android.widget.TextView
 import androidx.activity.ComponentActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.ViewGroupCompat
+import androidx.core.view.WindowInsetsCompat
 import com.example.androidpracticumcustomview.ui.theme.CustomContainer
 
-
 class XmlActivity : ComponentActivity() {
+    private var addSecondViewHandler = Handler(Looper.getMainLooper())
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        startXmlPracticum()
+        startXmlPracticum(savedInstanceState)
     }
 
-    private fun startXmlPracticum() {
+    private fun startXmlPracticum(savedInstanceState: Bundle?) {
         val customContainer = CustomContainer(this)
         setContentView(customContainer)
+        supportEdgeToEdgeScreen(customContainer)
         customContainer.setOnClickListener {
             finish()
         }
-
+        customContainer.setAnimationEnabled(savedInstanceState == null)
         val firstView = TextView(this).apply {
-            // TODO
-            // ...
+            text = getString(R.string.custom_view_group_first_view_name)
+            textSize = 20f
         }
-
         val secondView = TextView(this).apply {
-            // TODO
-            // ...
+            text = getString(R.string.custom_view_group_second_view_name)
+            textSize = 20f
         }
-
-        // Добавление второго элемента через некоторое время (например, по задержке)
-        Handler(Looper.getMainLooper()).postDelayed({
+        customContainer.addView(firstView)
+        if (savedInstanceState != null) {
             customContainer.addView(secondView)
-        }, 2000)
+        } else {
+            addSecondViewHandler.postDelayed({
+                customContainer.addView(secondView)
+            }, 2000)
+        }
+    }
+
+    private fun supportEdgeToEdgeScreen(view: View) {
+        ViewGroupCompat.installCompatInsetsDispatch(view)
+        ViewCompat.setOnApplyWindowInsetsListener(view) { v, insets ->
+            val innerPadding = insets.getInsets(
+                WindowInsetsCompat.Type.systemBars()
+                        or WindowInsetsCompat.Type.displayCutout()
+            )
+            v.setPadding(
+                innerPadding.left,
+                innerPadding.top,
+                innerPadding.right,
+                innerPadding.bottom
+            )
+            WindowInsetsCompat.CONSUMED
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        addSecondViewHandler.removeCallbacksAndMessages(null)
     }
 }
